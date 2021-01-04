fee354a56b6b1457ced6802ba5e24c4525ee5486
Megan Campbell <megan.campbell26@yahoo.com>
2021-01-04 10:35:41 -0700

    add foreign key

--- db/migrate/20210104164316_create_join_table_studios_movies.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableStudiosMovies < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :studios, :movies do |t|
-      t.index [:studio_id, :movie_id]
-      t.index [:movie_id, :studio_id]
-    end
-  end
-end
