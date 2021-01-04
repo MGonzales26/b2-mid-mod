879ba69c211a30e7e88f591c1851af5515af6bae
Megan Campbell <megan.campbell26@yahoo.com>
2021-01-04 11:08:21 -0700

    Finish story 1

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -6,17 +6,17 @@ describe "Movies Index Page" do
       disney = Studio.create!(name: 'Disney', location: 'Burbank, CA')
       universal = Studio.create!(name: 'Universal Studios', location: 'Universal City, CA')
 
-      beauty = Movie.create!(title: 'Beauty and the Beast', creation_year: 1991, genre: 'Family/Kids', :studio_id disney.id)
-      mummy = Movie.create!(title: 'The Mummy', creation_year: 1999, genre: 'Action-Adventure', :studio_id universal.id)
+      beauty = Movie.create!(title: 'Beauty and the Beast', creation_year: 1991, genre: 'Family/Kids', studio_id: disney.id)
+      mummy = Movie.create!(title: 'The Mummy', creation_year: 1999, genre: 'Action-Adventure', studio_id: universal.id)
 
       visit "/studios"
 
       expect(page).to have_content(disney.name)
       expect(page).to have_content(disney.location)
-      expect(page).to have_content(beauty.name)
+      expect(page).to have_content(beauty.title)
       expect(page).to have_content(universal.name)
       expect(page).to have_content(universal.location)
-      expect(page).to have_content(mummy.name)
+      expect(page).to have_content(mummy.title)
     end
   end
 end
